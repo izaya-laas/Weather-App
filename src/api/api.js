df20e31f2a21adf1d77066b58851cb2cd76c40b3
Lautaro Asat <lautaronorielasat@gmail.com>
2023-07-04 09:44:11 -0300

    Fix export api

--- src/api/api.js
+++ src/api/api.js
@@ -1,2 +1,4 @@
 const key = import.meta.env.VITE_API_KEY;
-export default api = `https://api.weatherapi.com/v1/forecast.json?key=${key}&lang=es&days=6&`;
+const api = `https://api.weatherapi.com/v1/forecast.json?key=${key}&lang=es&days=6&`;
+
+export default api;
